3b39f4ebcaba8dc4973bb8c064bea1e19a494263
johnburriss <38439336+johnburriss@users.noreply.github.com>
2019-04-19 11:32:49 -0400

    Create RemoteGPIpdate.ps1
    Runs a remote GP update on a machine as the current user logged in. Waits. Then runs a GP remote GP result and opens the report to ensure that the policy was applied correctly.

---
+++ RemoteGPUpdate.ps1
@@ -0,0 +1,12 @@
+$Computer = Read-host -Prompt "Enter the FQDN of Host"
+$file = $computer.split(".")[0] 
+
+$user = Get-WmiObject –ComputerName $computer –Class Win32_ComputerSystem | Select -ExpandProperty UserName
+
+Invoke-GPUpdate -Computer "$Computer"
+
+Start-Sleep -s 10
+
+Get-GPResultantSetOfPolicy -ReportType html -Computer $Computer -User "$user" -Path "c:\temp\$file.html"
+
+Invoke-Item "c:\temp\$file.html"
